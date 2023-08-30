e75f8d03ba3a9cdc5479de1395eac0218928d1e8
root <root@LAPTOP-01SJFTC3.localdomain>
2023-08-30 16:48:02 +0300

    feat: complete exercise 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,22 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) =>
+	<>
+		{parts.map((part) => 
+			<Part key={part.id} part={part}/>
+		)}    
+	</>
+
+
+const Course = ({ course }) => 
+	<>
+		<Header course={course.name}/>
+		<Content parts={course.parts}/>
+	</>
+
+export default Course
