9c3441159af1aa845544d2b95ad367d1afb8bb36
root <root@LAPTOP-01SJFTC3.localdomain>
2023-08-30 20:49:50 +0300

    feat: complete exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,13 +4,14 @@ const Number = ({ person }) => <div>{person.name} {person.number}</div>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addName = (event) => {
     event.preventDefault();
@@ -27,6 +28,7 @@ const App = () => {
 
     setNewName('')
     setNewNumber('')
+    setFilter('')
   }
 
   const handleNameChange = (event) => {
@@ -37,9 +39,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterName = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with <input 
+                          value={filter}
+                          onChange = {handleFilterName}  
+                        />
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={addName}>
         <div>
           name: <input
@@ -57,7 +70,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <Number key={person.name} person={person}/>)}
+      {persons.filter((person) => person.name.toLowerCase().match(RegExp(`.*${filter.toLowerCase()}.*`))).map((person) => <Number key={person.name} person={person}/>)}
     </div>
   )
 }
