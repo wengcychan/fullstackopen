60f00ac372344b4e034efd9798bc29a9d6173ed1
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-14 23:42:27 +0300

    feat: complete exercises 4.15 - 4.16

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+require('express-async-errors')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogRoute = require('./controllers/blogs')
+const userRoute = require('./controllers/users')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(config.MONGODB_URI)
 
@@ -11,5 +14,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRoute)
+app.use('/api/users', userRoute)
+
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,6 +3,8 @@ const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 beforeEach(async () => {
 	await Blog.deleteMany({})
@@ -130,6 +132,121 @@ test('a blog is updated', async() => {
 	expect(blogAtEnd).toHaveLength(helper.initialBlogs.length)
 
 	expect(blogAtEnd[0].likes).toBe(8)
+})
+
+describe('when there is initially one user in db', () => {
+
+	beforeEach(async () => {
+		await User.deleteMany({})
+
+		const passwordHash = await bcrypt.hash('sekret', 10)
+		const user = new User({ username: 'root', passwordHash })
+
+		await user.save()
+	})
+
+	test('creation fails with proper statuscode and message if the username is less than 3 characters long', async() => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'a',
+			name: 'a',
+			password: '12345'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+
+		expect(result.body.error).toContain('username: Path `username` (`a`) is shorter than the minimum allowed length (3).')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toEqual(usersAtStart)
+	})
+
+	test('creation fails with proper statuscode and message if the password is less than 3 characters long', async() => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'abc',
+			name: 'abc',
+			password: '12'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+
+		expect(result.body.error).toContain('password must be at least 3 characters long')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toEqual(usersAtStart)
+	})
+
+	test('creation fails with proper statuscode and message if username is missing', async() => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			name: 'abc',
+			password: '12345'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+
+		expect(result.body.error).toContain('username: Path `username` is required')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toEqual(usersAtStart)
+	})
+
+	test('creation fails with proper statuscode and message if password is missing', async() => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'abc',
+			name: 'abc'
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+
+		expect(result.body.error).toContain('missing password')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toEqual(usersAtStart)
+	})
+
+	test('creation fails with proper statuscode and message if username already taken', async() => {
+
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'root',
+			name: 'Superuser',
+			password: 'salainen',
+		}
+
+		const result = await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(400)
+
+		expect(result.body.error).toContain('expected `username` to be unique')
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toEqual(usersAtStart)
+	})
 })
 
 afterAll(async () => {
