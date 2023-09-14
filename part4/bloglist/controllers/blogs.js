23d77ae6961ec9f8062a58a1cd7b655cc80f2137
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-15 00:14:57 +0300

    feat: complete exercise 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogRoute = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRoute.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
 	response.json(blogs)
 })
 
@@ -15,9 +16,21 @@ blogRoute.post('/', async (request, response) => {
 	if (!body.likes)
 		body.likes = 0
 
-	const blog = new Blog(body)
+	const user = await User.findOne()
+
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		user: user.id,
+		likes: body.likes
+	})
 
 	const savedBlog = await blog.save()
+
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
+
 	response.status(201).json(savedBlog)
 })
 
