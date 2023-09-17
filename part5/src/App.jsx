054bd235bfda062f2bb533312ec79f0e7a2c6ba7
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-17 14:06:57 +0300

    feat: complete exercise 5.2

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -15,17 +15,30 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception) {
       console.log(exception)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
   }
 
   const loginForm = () => (
@@ -61,8 +74,13 @@ const App = () => {
     return loginForm()
   return (
     <div>
-    <h2>blogs</h2>
-    <p>{user.name} logged in</p>
+      <h2>blogs</h2>
+      <form onClick={handleLogout}>
+        {user.name} logged in
+        <button type="click">
+          logout
+        </button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
