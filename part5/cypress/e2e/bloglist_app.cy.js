ae2c9e1ca8d444a1d4b47d792408ae0537973944
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-19 14:55:59 +0300

    feat: complete exercise 5.18

--- part5/cypress/e2e/bloglist_app.cy.js
+++ part5/cypress/e2e/bloglist_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -8,5 +14,24 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+    })
   })
 })
