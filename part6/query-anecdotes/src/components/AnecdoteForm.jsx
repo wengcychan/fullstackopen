561bb6de2ac0f265ad13a57055f2d17cab52e19c
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-21 16:19:17 +0300

    feat: complete exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,25 @@
+import { createAnecdote } from '../request'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+
 const AnecdoteForm = () => {
+
+  const queryClient = useQueryClient()
+
+  const newMutationAnecdote = useMutation({ 
+    mutationFn: createAnecdote,     
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData({ queryKey: ['anecdotes'] })
+      queryClient.setQueryData({ queryKey: ['anecdotes'] }, anecdotes.concat(newAnecdote))
+    }}
+  )
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    if (content.length >= 5)
+      newMutationAnecdote.mutate({ content, votes: 0 })
 }
 
   return (
